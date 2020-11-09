7cbb00f2448c8ff27024f472d9db81f5bfeaa1c7
mhio <37205532+mhio@users.noreply.github.com>
2020-11-10 10:08:27 +1100

    modules test

--- test/built/module.js
+++ test/built/module.js
@@ -1,15 +1,19 @@
 /* global expect */
 
-const { KoaApiHandle, KoaApiHandleException } = require('../../')
+const { KoaGenericHandle, KoaGenericHandleException } = require('../../')
 
 describe('mh::test::built::module', function(){
 
-  it('should load the KoaApiHandle', function(){
-    expect( KoaApiHandle, 'KoaApiHandle module' ).to.be.ok
+  it('should load the KoaGenericHandle', function(){
+    expect( KoaGenericHandle, 'KoaGenericHandle module' ).to.be.ok
   })
 
-  it('should load KoaApiHandleException', function(){
-    expect( KoaApiHandleException, 'KoaApiHandleException module' ).to.be.ok
+  it('should load KoaGenericHandleException', function(){
+    expect( KoaGenericHandleException, 'KoaGenericHandleException module' ).to.be.ok
+  })
+
+  it('should return a logging function', function(){
+    expect( KoaGenericHandle.logging() ).to.be.a('function')
   })
 
 })
