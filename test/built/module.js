317e290a4d391cc5a9c958880e7cc807f903647c
mhio <37205532+mhio@users.noreply.github.com>
2020-11-10 10:13:13 +1100

    finish removing exception

--- test/built/module.js
+++ test/built/module.js
@@ -1,15 +1,11 @@
 /* global expect */
 
-const { KoaGenericHandle, KoaGenericHandleException } = require('../../')
+const { KoaGenericHandle } = require('../../')
 
 describe('mh::test::built::module', function(){
 
   it('should load the KoaGenericHandle', function(){
     expect( KoaGenericHandle, 'KoaGenericHandle module' ).to.be.ok
-  })
-
-  it('should load KoaGenericHandleException', function(){
-    expect( KoaGenericHandleException, 'KoaGenericHandleException module' ).to.be.ok
   })
 
   it('should return a logging function', function(){
